8147da5a658f67672f850d2447424a1f1c219623
Chandni Varshney <chandnivarshney07@gmail.com>
2025-09-19 17:15:27 +0530

    all setup configured

--- src/app.js
+++ src/app.js
@@ -17,6 +17,15 @@ app.use(cookieParser())
 
 
 
+// routes import
+import userRouter from './routes/user.routes.js'
 
 
-export { app } 
+
+//  routes declaration
+app.use("/api/v1/users", userRouter)
+
+// http://localhost:8000/api/v1/users/register
+
+
+export { app } ;
--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,10 +1,10 @@
-const asyncHandler =  (requesthandler) => {
-    (req, res, next) => {
+const asyncHandler =  (requestHandler) => {
+    return (req, res, next) => {
         Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err))
     }
 }
 
-export {asyncHandler}
+export { asyncHandler }
 
 
 
